d441dd8a30d5faa35fa0cedf1a8e19a0bd9bb702
Azmatkhan12 <rehan.dgms@gmail.com>
2022-11-09 10:20:23 +0530

    secons step

--- src/App.js
+++ src/App.js
@@ -3,18 +3,61 @@ import Expense from "./Components/Expense";
 
 
 function App(){
+   const expense =[
+    {
+        id :'exp1',
+        date: new Date(2022,11,25),
+        title: 'Home Loan',
+        amount : 30000
 
-    let expenesDate = new Date(2022,11,9);
-    let expenesTitle = "Home Loan";
-    let expenesAmount = 300;
+    },
+    {
+        id :'exp2',
+        date: new Date(2022,11,5),
+        title: 'Phone EMI',
+        amount :3000 
+
+    },
+    {
+        id :'exp3',
+        date: new Date(2022,11,10),
+        title: 'Room Rent',
+        amount : 4000
+
+    },
+    {
+        id :'exp4',
+        date: new Date(2022,11,1),
+        title: 'Mesh Bill',
+        amount : 8500
+
+    }
+   ];
     return (
         <div>
             <h1>Expense Tracker</h1>
              <Expense 
-             date = {expenesDate} 
-             title = {expenesTitle}
-             amount = {expenesAmount}>
+             date = {expense[0].date} 
+             title = {expense[0].title}
+             amount = {expense[0].amount}>
 
+             </Expense>
+             <Expense 
+             date = {expense[1].date} 
+             title = {expense[1].title}
+             amount = {expense[1].amount}>
+
+             </Expense>
+             <Expense 
+             date = {expense[2].date} 
+             title = {expense[2].title}
+             amount = {expense[2].amount}>
+
+             </Expense>
+             <Expense 
+             date = {expense[3].date} 
+             title = {expense[3].title}
+             amount = {expense[3].amount}>
              </Expense>
 
         </div>
